5d7e9b873213624efc6255ba817d7e84aa381a5b
Tadas Petra <tadas@tadaspetra.com>
2025-10-24 10:04:59 -0500

    testing new deployment

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="dark flex flex-col w-full max-w-5xl mx-auto">
       <p className="text-center text-white mb-4">
-        Environment Test: {process.env.NEXT_PUBLIC_TEST || "Not set"}
+        Environment Test CHANGE: {process.env.NEXT_PUBLIC_TEST || "Not set"}
       </p>
     </div>
   );
